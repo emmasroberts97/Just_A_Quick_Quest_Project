cf63ef58a680ea9ce4157a14554294414f9edf63
JohnPooley <thevinylvandal@hotmail.com>
2020-04-09 23:36:19 +0100

    controllers added for characters, questionMasters. Get and Post methods working. NO DELETE METHODS WORKING CURRENTLY. DB TABLE SET TO "create-drop"

---
+++ src/main/java/com/codeclan/example/justaquickquest/controllers/TreasureController.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.justaquickquest.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/treasures")
+public class TreasureController {
+
+//   not sure how to do this for eNums
+}
