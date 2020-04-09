bdedc3fa3e1eb8adad94e7575113a536a524905b
Emma Roberts <emma.roberts197@gmail.com>
2020-04-09 14:54:42 +0100

    Initial Commit

---
+++ src/main/java/com/codeclan/example/justaquickquest/JustaquickquestApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.justaquickquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustaquickquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustaquickquestApplication.class, args);
+	}
+
+}
