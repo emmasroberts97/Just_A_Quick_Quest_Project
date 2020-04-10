2b11e55b2f62ec3abba0b08e4756e8a195f3aed2
Jack Portwood <Jack.Portwood14@gmail.com>
2020-04-10 14:48:53 +0100

    API fecth requet started and helpers container started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
+import QuestContainer from './containers/QuestContainer';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    <h1>Hello</h1>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <QuestContainer/>
+    );
+  } 
+  
 }
 
 export default App;
