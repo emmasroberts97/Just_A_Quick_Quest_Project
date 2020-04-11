70d864fbbe0f8ff7752e50e3176a3bb8508c4519
Emma Roberts <emma.roberts197@gmail.com>
2020-04-11 11:37:35 +0100

    added random functionality

--- src/containers/QuestContainer.js
+++ src/containers/QuestContainer.js
@@ -12,8 +12,9 @@ class QuestContainer extends Component{
             result: "",
             selectedRoom: "",
             selectedQuestionMaster: "",
-            selectedTreasure: ""
-
+            selectedTreasure: "",
+            selectedQuestion: "",
+            selectedDifficulty: ""
         }
     }
 
@@ -22,25 +23,45 @@ class QuestContainer extends Component{
         .then(res => res.json())
         .then((data)=>{
             this.setState({questions: data.results});
-        })
+            this.returnRandomQuestion()})
 
         const request1 = new Request();
         request1.get('/api/rooms')
             .then((data) => {
                 this.setState({ rooms: data })
-            })
+                this.returnRandomRoom()})
 
         const request2 = new Request();
         request2.get('/api/questionmasters')
             .then((data) => {
                 this.setState({ questionMasters: data })
-            })
+                this.returnRandomQuestionMaster()})
 
         const request3 = new Request();
         request3.get('/api/treasures')
             .then((data) => {
                 this.setState({ treasure: data })
-            })
+                this.returnRandomTreasure()})
+    }
+
+    returnRandomRoom(){
+        const randomRoom = this.state.rooms[Math.floor(Math.random()*this.state.rooms.length)];
+        this.setState({selectedRoom: randomRoom});
+    }
+
+    returnRandomQuestionMaster(){
+        const randomQuestionMaster = this.state.questionMasters[Math.floor(Math.random()*this.state.questionMasters.length)];
+        this.setState({selectedQuestionMaster: randomQuestionMaster});
+    }
+
+    returnRandomTreasure(){
+        const randomTreasure = this.state.treasure[Math.floor(Math.random()*this.state.treasure.length)];
+        this.setState({selectedTreasure: randomTreasure});
+    }
+
+    returnRandomQuestion(){
+        const randomQuestion = this.state.questions[Math.floor(Math.random()*this.state.questions.length)];
+        this.setState({selectedQuestion: randomQuestion});
     }
 
     render(){
